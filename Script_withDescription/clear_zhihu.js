99a10365d306c67d07abfdf3c2ca209a320dad1d
_YL_ <dragonlinkailong@gmail.com>
2019-12-20 13:06:33 +0800

    Update clear_zhihu.js

--- Script_withDescription/clear_zhihu.js
+++ Script_withDescription/clear_zhihu.js
@@ -13,11 +13,16 @@ var delay_whole = 700;
 var delay_click = 10;
 // All classes that need to clear
 var block_classes = ['MobileAppHeader-downloadLink', 'MobileAppHeader-authLink', 'HotBanner AdHotBanner', 'OpenInAppButton OpenInApp is-shown',
-        'Button ViewAllInappButton Button--primary Button--blue', 'Button Button--primary Button--blue',
-        'Input', 'MobileAppHeader-searchBox MobileAppHeader-searchBoxWithUnlogin Input-wrapper', 
-        'Card RelatedReadings', 'Card HotQuestions',
-        'HotQuestionsItem', 'HotQuestions-content', 'HotQuestions-title', 'HotQuestionsItem HotQuestionsItem-mul', 'HotQuestions-bottomButton',
-        'MHotFeedAd-content', 'MHotFeedAd', 'MBannerAd'];
+            'Button ViewAllInappButton Button--primary Button--blue', 'Button Button--primary Button--blue',
+            'Input', 'MobileAppHeader-searchBox MobileAppHeader-searchBoxWithUnlogin Input-wrapper', 
+            'Card RelatedReadings', 'Card HotQuestions', 'Card ViewAllInappCard', 
+            'HotQuestionsItem', 'HotQuestions-content', 'HotQuestions-title', 'HotQuestionsItem HotQuestionsItem-mul', 'HotQuestions-bottomButton',
+            'MHotFeedAd-content', 'MHotFeedAd', 'MBannerAd',
+            'Banner-image', 'AppBanner-layout', 'Button AppHeader-login Button--blue', 'Tabs-link AppHeader-TabsLink', 'SearchBar-input Input-wrapper Input-wrapper--grey', 
+            'Card-section RelatedCommodities-list', 'Card-header RelatedCommodities-title', 
+            'Card-section SimilarQuestions-list', 'Card-header SimilarQuestions-title',
+            'Card-section AnswerInfo-favlists', 'Card-header AnswerInfo-title',
+            'Button Button--blue', 'Button Button--grey Button--withIcon Button--withLabel', 'Button Button--plain Button--withIcon Button--withLabel', 'QuestionHeaderActions'];
 
 setTimeout(function () {
     for(var i = 0; i < block_classes.length; i++){
